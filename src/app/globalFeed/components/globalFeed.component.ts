13ac71f28944ba4a4fccf80333105dd938ddfb8e
MickaelBoisDev <mickaelbois.dev@gmail.com>
2023-07-24 17:18:23 +0200

    Adding feed toggler
    Feed toggler component for user/global/tag toggle

--- src/app/globalFeed/components/globalFeed.component.ts
+++ src/app/globalFeed/components/globalFeed.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BannerComponent } from 'src/app/shared/components/banner/banner.component';
 import { ErrorMessageComponent } from 'src/app/shared/components/errorMessage/errorMessage.component';
 import { FeedComponent } from 'src/app/shared/components/feed/feed.component';
+import { feedTogglerComponent } from 'src/app/shared/components/feedToggler/feedToggler.component';
 import { PopularTagsComponent } from 'src/app/shared/components/popularTags/popularTags.component';
 
 @Component({
@@ -13,6 +14,7 @@ import { PopularTagsComponent } from 'src/app/shared/components/popularTags/popu
     BannerComponent,
     ErrorMessageComponent,
     PopularTagsComponent,
+    feedTogglerComponent,
   ],
 })
 export class GlobalFeedComponent {
