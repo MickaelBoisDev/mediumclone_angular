ac5bd0163b2d93f6b1bcfab53ad684865439c059
MickaelBoisDev <mickaelbois.dev@gmail.com>
2023-07-24 15:09:21 +0200

    Creating pagination
    Add on shared : Pagination components / Utils services

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -4,15 +4,23 @@ import { feedActions } from './store/actions';
 import { combineLatest } from 'rxjs';
 import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { ErrorMessageComponent } from '../errorMessage/errorMessage.component';
 import { LoadingComponent } from '../loading/loading.component';
+import { environment } from 'src/environments/environment.development';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   standalone: true,
-  imports: [CommonModule, RouterLink, ErrorMessageComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = '';
@@ -22,10 +30,24 @@ export class FeedComponent implements OnInit {
     error: this.store.select(selectError),
     feed: this.store.select(selectFeedData),
   });
+  limit = environment.limit;
+  baseUrl = this.router.url.split('?')[0];
+  currentPage: number = 0;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      console.log('params :', params);
+      this.currentPage = Number(params['page'] || '1');
+      this.fetchFeed();
+    });
+  }
+  fetchFeed(): void {
     this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }));
   }
 }
---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { UtilsService } from './../../services/utils.service';
+import { Component, Input, OnInit } from '@angular/core';
+@Component({
+  selector: 'mc-pagination',
+  templateUrl: './pagination.component.html',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+})
+export class PaginationComponent implements OnInit {
+  @Input() total: number = 0;
+  @Input() limit: number = 5;
+  @Input() currentPage: number = 1;
+  @Input() url: string = '';
+
+  pagesCount: number = 1;
+  pages: number[] = [];
+
+  constructor(private utilsService: UtilsService) {}
+  ngOnInit(): void {
+    this.pagesCount = Math.ceil(this.total / this.limit);
+    this.pages =
+      this.pagesCount > 0 ? this.utilsService.range(1, this.pagesCount) : [];
+    console.log(this.pages);
+  }
+}
